bec8f06be6f7f2a064fab9de23a2e6130bad08a6
Lord-Giganticus <mrubins64@gmail.com>
2021-11-10 14:37:00 -0600

    Start BMG work

---
+++ LordG.IO/BMGUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Syroot.BinaryData;
+using System.Xml;
+
+namespace LordG.IO
+{
+    public static class BMGUtil
+    {
+        public const ulong MagicBE = 0x4D455347626D6731;
+
+        public const ulong MagicLE = 0x31676D624753454D;
+
+        public static bool CheckMagic(byte[] src, ByteOrder order)
+        {
+            using (EndianStream stream = src)
+            {
+                var num = stream.ReadULong(order);
+                return num is MagicBE || num is MagicLE;
+            }
+        }
+
+        
+    }
+}
